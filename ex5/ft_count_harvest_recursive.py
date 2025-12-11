7b52dd89fb803d4373e597600c43f3094af85edb
DESKTOP-RM1SGF3\Elias <eliaspowder@gmail.com>
2025-12-11 01:35:14 +0100

    ex5, ex6, ex7

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,11 +1,10 @@
-i = 0
+def counter(count, max):
+	print("Day", count)
+	if count == max:
+		return
+	counter(count + 1, max)
 
 def ft_count_harvest_recursive():
-	if i == 0:
-		i = int(input())
-	print("Day", i)
-	i -= 1
-	if i == 0:
-		print("Harvest time!")
-		return
-	ft_count_harvest_recursive()
+	print("Days until harvest:", end=" ")
+	counter(1, int(input()))
+	print("Harvest time!")
