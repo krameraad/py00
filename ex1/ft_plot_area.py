55606d09267806698295cef2b136c17fab1a238d
krameraad <96356397+krameraad@users.noreply.github.com>
2026-01-08 01:33:51 +0100

    ex1: condensed print/input

--- ex1/ft_plot_area.py
+++ ex1/ft_plot_area.py
@@ -1,6 +1,4 @@
 def ft_plot_area():
-    print("Enter length:", end=" ")
-    length = int(input())
-    print("Enter width:", end=" ")
-    width = int(input())
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
     print("Plot area:", length * width)
